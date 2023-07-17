bbcfee81cb18ce28c36cf164c54e22424063d8a2
Guen Prawiroatmodjo <guenp@microsoft.com>
2023-07-16 20:18:25 -0500

    add telemetry module to scipy._lib

--- scipy/_lib/telemetry.py
+++ scipy/_lib/telemetry.py
@@ -1,5 +1,7 @@
 import os
 
+from typing import Sequence
+
 from opentelemetry import trace
 from opentelemetry.sdk.resources import Resource
 from opentelemetry.sdk.trace import TracerProvider
@@ -9,6 +11,10 @@ from opentelemetry.exporter.jaeger.thrift import JaegerExporter
 from opentelemetry.sdk.resources import SERVICE_NAME, Resource
 
 from contextlib import wraps
+
+ALLOWED_TYPES = [bool, str, bytes, int, float]
+
+__all__ = ["span"]
 
 provider = TracerProvider()
 processor = BatchSpanProcessor(ConsoleSpanExporter())
@@ -29,18 +35,37 @@ trace.get_tracer_provider().add_span_processor(
   BatchSpanProcessor(jaeger_exporter)
 )
 
+
+def _get_func_name(func):
+    return f"{func.__module__}.{func.__qualname__}"
+
+
+def _serialize(arg):
+    for _type in ALLOWED_TYPES:
+        if isinstance(arg, _type):
+            return arg
+        if isinstance(arg, Sequence) and len(arg) > 0:
+            if isinstance(arg[0], _type):
+                return arg
+    return str(arg)
+
+
 def span(func):
     # Creates a tracer from the global tracer provider
     tracer = trace.get_tracer(__name__)
+    func_name = _get_func_name(func)
     
     @wraps(func)
-    def inner(*args, **kwargs):
+    def span_wrapper(*args, **kwargs):
         print(args, kwargs)
-        with tracer.start_as_current_span(func.__name__) as span:
+        import inspect
+        stack = inspect.stack()
+        print([stack[n][3] for n in range(len(stack))])
+        with tracer.start_as_current_span(func_name) as span:
             for n, arg in enumerate(args):
-                span.set_attribute(f"arg{n}", arg)
+                span.set_attribute(f"args.{n}", _serialize(arg))
             for k, v in kwargs.items():
-                span.set_attribute(k, v)
+                span.set_attribute(f"kwargs.{k}", v)
             return func(*args, **kwargs)
 
-    return inner
+    return span_wrapper
